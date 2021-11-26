61a94c952007b2159bf778b65be99474a3ee5b3e
lupoglaz <georgy.derevyanko@gmail.com>
2021-11-26 16:05:09 +0300

    Small changes to debug

--- af2_debug.py
+++ af2_debug.py
@@ -15,13 +15,15 @@ def test_wrapper(filename, _forward_fn):
 	apply = hk.transform(_forward_fn).apply
 	init = hk.transform(_forward_fn).init
 	
-	params = hk.data_structures.to_mutable_dict(init(rng, feat))
+	params = init(rng, feat)
+	params = hk.data_structures.to_mutable_dict(params)
+	
 	res = apply(params, rng, feat)
 
 	res_path = Path('Debug')/(f'{filename}.pkl')
 	with open(res_path, 'wb') as f:
 		pickle.dump((feat, params, res), f)
-
+	
 
 if __name__=='__main__':
 	model_config = config.model_config('model_1')
@@ -42,6 +44,10 @@ if __name__=='__main__':
 
 	
 	feat = {
+	# 'q_data': jnp.zeros((batch_size, N_queries, q_channels), dtype=jnp.float32),
+	# 'm_data': jnp.zeros((batch_size, N_keys, m_channels), dtype=jnp.float32),
+	# 'bias': jnp.zeros((batch_size, 1, N_queries, N_keys), dtype=jnp.float32),
+	# 'nonbatched_bias': jnp.zeros((N_queries, N_keys), dtype=jnp.float32),
 	'q_data': jax.random.normal(rng, (batch_size, N_queries, q_channels), dtype=jnp.float32),
 	'm_data': jax.random.normal(rng, (batch_size, N_keys, m_channels), dtype=jnp.float32),
 	'bias': jax.random.normal(rng, (batch_size, 1, N_queries, N_keys), dtype=jnp.float32),
@@ -56,7 +62,7 @@ if __name__=='__main__':
 
 	conf = model_config.model.embeddings_and_evoformer.evoformer.msa_row_attention_with_pair_bias
 	global_config = model_config.model.global_config
-
+	# conf.gating = False
 	# test_wrapper('Attention',
 	# 	lambda batch:
 	# 		Attention(conf, global_config, output_dim=256)
@@ -89,24 +95,24 @@ if __name__=='__main__':
 	# 	)
 
 	
-	conf = model_config.model.embeddings_and_evoformer.evoformer.triangle_attention_starting_node
-	test_wrapper('TriangleAttention',
-		lambda batch:
-			TriangleAttention(conf, global_config)
-			(pair_act=batch['pair_act'], pair_mask=batch['pair_mask'], is_training=False)
-		)
-	conf = model_config.model.embeddings_and_evoformer.evoformer.triangle_multiplication_outgoing
-	test_wrapper('TriangleMultiplication',
-		lambda batch:
-			TriangleMultiplication(conf, global_config)
-			(act=batch['pair_act'], mask=batch['pair_mask'], is_training=False)
-		)
-	conf = model_config.model.embeddings_and_evoformer.evoformer.outer_product_mean
-	test_wrapper('OuterProductMean',
-		lambda batch:
-			OuterProductMean(conf, global_config, num_output_channel=256)
-			(act=batch['msa_act'], mask=batch['msa_mask'], is_training=False)
-		)
+	# conf = model_config.model.embeddings_and_evoformer.evoformer.triangle_attention_starting_node
+	# test_wrapper('TriangleAttention',
+	# 	lambda batch:
+	# 		TriangleAttention(conf, global_config)
+	# 		(pair_act=batch['pair_act'], pair_mask=batch['pair_mask'], is_training=False)
+	# 	)
+	# conf = model_config.model.embeddings_and_evoformer.evoformer.triangle_multiplication_outgoing
+	# test_wrapper('TriangleMultiplication',
+	# 	lambda batch:
+	# 		TriangleMultiplication(conf, global_config)
+	# 		(act=batch['pair_act'], mask=batch['pair_mask'], is_training=False)
+	# 	)
+	# conf = model_config.model.embeddings_and_evoformer.evoformer.outer_product_mean
+	# test_wrapper('OuterProductMean',
+	# 	lambda batch:
+	# 		OuterProductMean(conf, global_config, num_output_channel=256)
+	# 		(act=batch['msa_act'], mask=batch['msa_mask'], is_training=False)
+	# 	)
 	
 	
 	

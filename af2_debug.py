a55f30fac3dda3f1e7e94da2b55e2e1de49917f3
lupoglaz <georgy.derevyanko@gmail.com>
2021-12-01 18:37:25 +0300

    Debuggin evoformerandemb

--- af2_debug.py
+++ af2_debug.py
@@ -8,22 +8,9 @@ import pickle
 from pathlib import Path
 
 from alphafold.model.modules import Attention, GlobalAttention, MSARowAttentionWithPairBias, MSAColumnAttention, MSAColumnGlobalAttention
-from alphafold.model.modules import TriangleAttention, TriangleMultiplication, OuterProductMean
+from alphafold.model.modules import TriangleAttention, TriangleMultiplication, OuterProductMean, Transition
+from alphafold.model.modules import EvoformerIteration, EmbeddingsAndEvoformer
 from alphafold.model import config
-
-def test_wrapper(filename, _forward_fn):
-	apply = hk.transform(_forward_fn).apply
-	init = hk.transform(_forward_fn).init
-	
-	params = init(rng, feat)
-	params = hk.data_structures.to_mutable_dict(params)
-	
-	res = apply(params, rng, feat)
-
-	res_path = Path('Debug')/(f'{filename}.pkl')
-	with open(res_path, 'wb') as f:
-		pickle.dump((feat, params, res), f)
-	
 
 if __name__=='__main__':
 	model_config = config.model_config('model_1')
@@ -58,7 +45,23 @@ if __name__=='__main__':
 	'msa_mask': jax.random.bernoulli(rng, 0.5, (N_seq, N_res)),
 	'pair_act': jax.random.normal(rng, (N_res, N_res, c_z), dtype=jnp.float32),
 	'pair_mask': jax.random.bernoulli(rng, 0.5, (N_res, N_res)),
+
+	'seq_act': jax.random.normal(rng, (batch_size, N_res, q_channels), dtype=jnp.float32),
+	'seq_mask': jax.random.bernoulli(rng, 0.5, (batch_size, N_res)),
 	}
+	
+	def test_wrapper(filename, _forward_fn):
+		apply = hk.transform(_forward_fn).apply
+		init = hk.transform(_forward_fn).init
+		
+		params = init(rng, feat)
+		params = hk.data_structures.to_mutable_dict(params)
+		
+		res = apply(params, rng, feat)
+
+		res_path = Path('Debug')/(f'{filename}.pkl')
+		with open(res_path, 'wb') as f:
+			pickle.dump((feat, params, res), f)
 
 	conf = model_config.model.embeddings_and_evoformer.evoformer.msa_row_attention_with_pair_bias
 	global_config = model_config.model.global_config
@@ -107,12 +110,86 @@ if __name__=='__main__':
 	# 		TriangleMultiplication(conf, global_config)
 	# 		(act=batch['pair_act'], mask=batch['pair_mask'], is_training=False)
 	# 	)
+
+	# feat['msa_act'] = jax.random.normal(rng, (N_seq, N_res, 16), dtype=jnp.float32)
+	# feat['msa_mask'] = jax.random.bernoulli(rng, 0.5, (N_seq, N_res))
 	# conf = model_config.model.embeddings_and_evoformer.evoformer.outer_product_mean
 	# test_wrapper('OuterProductMean',
 	# 	lambda batch:
 	# 		OuterProductMean(conf, global_config, num_output_channel=256)
 	# 		(act=batch['msa_act'], mask=batch['msa_mask'], is_training=False)
 	# 	)
+
+	# conf = model_config.model.embeddings_and_evoformer.evoformer.pair_transition
+	# test_wrapper('Transition',
+	# 	lambda batch:
+	# 		Transition(conf, global_config)
+	# 		(act=batch['seq_act'], mask=batch['seq_mask'], is_training=False)
+	# 	)
+
+	# conf = model_config.model.embeddings_and_evoformer.evoformer
+	# test_wrapper('EvoformerIteration1',
+	# 	lambda batch:
+	# 		EvoformerIteration(conf, global_config, is_extra_msa=False)
+	# 		(activations={'msa':batch['msa_act'], 'pair':batch['pair_act']}, 
+	# 		masks={'msa':batch['msa_mask'], 'pair':batch['pair_mask']}, is_training=False)
+	# 	)
+	# conf = model_config.model.embeddings_and_evoformer.evoformer
+	# test_wrapper('EvoformerIteration2',
+	# 	lambda batch:
+	# 		EvoformerIteration(conf, global_config, is_extra_msa=True)
+	# 		(activations={'msa':batch['msa_act'], 'pair':batch['pair_act']}, 
+	# 		masks={'msa':batch['msa_mask'], 'pair':batch['pair_mask']}, is_training=False)
+	# 	)
+
+	proc_features_path = Path('/media/lupoglaz/AlphaFold2Output')/Path('T1024')/Path('proc_features.pkl')
+	with open(proc_features_path, 'rb') as f:
+		feat = pickle.load(f)
+	feat = {k: v[0] for k, v in feat.items()}
+
+	num_msa = 300
+	num_seq = 400
+	num_extra_seq = 100
+	
+	feat['aatype'] = feat['aatype'][:num_seq]
+	feat['residue_index'] = feat['residue_index'][:num_seq]
+	feat['seq_length'] = np.int32(num_seq)
+	feat['seq_mask'] = feat['seq_mask'][:num_seq]
+	
+	feat['msa_mask'] = feat['msa_mask'][:num_msa, :num_seq]
+	feat['msa_row_mask'] = feat['msa_row_mask'][:num_msa]
+	
+
+	feat['template_aatype'] = feat['template_aatype'][:,:num_seq]
+	feat['template_all_atom_masks'] = feat['template_all_atom_masks'][:,:num_seq,:]
+	feat['template_all_atom_positions'] = feat['template_all_atom_positions'][:,:num_seq,:,:]
+	feat['template_pseudo_beta'] = feat['template_pseudo_beta'][:,:num_seq,:]
+	feat['template_pseudo_beta_mask'] = feat['template_pseudo_beta_mask'][:,:num_seq]
+	feat['atom14_atom_exists'] = feat['atom14_atom_exists'][:num_seq,:]
+	feat['residx_atom14_to_atom37'] = feat['residx_atom14_to_atom37'][:num_seq,:]
+	feat['residx_atom37_to_atom14'] = feat['residx_atom37_to_atom14'][:num_seq,:]
+	feat['atom37_atom_exists'] = feat['atom37_atom_exists'][:num_seq,:]
+	
+	
+	feat['extra_msa'] = feat['extra_msa'][:num_extra_seq, :num_seq]
+	feat['extra_msa_mask'] = feat['extra_msa_mask'][:num_extra_seq, :num_seq]
+	feat['extra_msa_row_mask'] = feat['extra_msa_row_mask'][:num_extra_seq]
+	feat['extra_has_deletion'] = feat['extra_has_deletion'][:num_extra_seq, :num_seq]
+	feat['extra_deletion_value'] = feat['extra_deletion_value'][:num_extra_seq, :num_seq]
+
+	feat['bert_mask'] = feat['bert_mask'][:num_msa, :num_seq]
+	feat['true_msa'] = feat['true_msa'][:num_msa, :num_seq]
+	feat['msa_feat'] = feat['msa_feat'][:num_msa, :num_seq, :]
+	feat['target_feat'] = feat['target_feat'][:num_seq, :]
+
+	for key in feat.keys():
+		print(key, feat[key].shape, feat[key].dtype)
+	
+	conf = model_config.model.embeddings_and_evoformer
+	test_wrapper('EmbeddingsAndEvoformer',
+		lambda batch:EmbeddingsAndEvoformer(conf, global_config)(batch, is_training=False)
+		)
+
 	
 	
 	
